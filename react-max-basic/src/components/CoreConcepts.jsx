13898d021b33312fd084665b538f1e68d683fc85
sting7401 <sting7401@naver.com>
2024-01-10 16:32:06 +0900

    core

---
+++ react-max-basic/src/components/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CoreConcept from : './CoreConcept'
+import { CORE_CONCEPTS } from './../data';
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+        <h2>core`</h2>
+        <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem} {...conceptItem} />))}
+        </ul>
+    </section>
+  )
+}
